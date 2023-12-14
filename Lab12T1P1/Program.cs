66e9cd805d481b3dc981f746fae0cae8394f56a3
user <user@WIN-T9NTGAQGALP>
2023-12-14 19:46:26 +0200

    Добавьте файлы проекта.

---
+++ Lab12T1P1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int mod = 23;
+
+        Console.WriteLine("Елiптична крива: y^2 = (x^3 + x + 1) mod 23");
+
+        for (int x = 0; x < mod; x++)
+        {
+            int ySquared = (int)((Math.Pow(x, 3) + x + 1) % mod);
+
+            int y = FindSquareRootModulo(ySquared, mod);
+
+            if (y != -1)
+            {
+                Console.WriteLine($"Точка ({x}, {y}) на елiптичнiй кривi.");
+            }
+        }
+    }
+
+    static int FindSquareRootModulo(int a, int p)
+    {
+        for (int x = 0; x < p; x++)
+        {
+            if ((x * x) % p == a)
+            {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+}
